203dd2e44cb421a9c6e124fd90ba575d54b1502e
GENIUS SAVAGE SHIV <83080405+SHIVGULSHAN@users.noreply.github.com>
2021-05-11 15:39:28 +0530

    Update ziptools.py

--- plugins/ziptools.py
+++ plugins/ziptools.py
@@ -1,9 +1,6 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# Aghori - UserBot
+# Copyright (C) 2020 TeamAghori
+
 
 """
 ✘ Commands Available
