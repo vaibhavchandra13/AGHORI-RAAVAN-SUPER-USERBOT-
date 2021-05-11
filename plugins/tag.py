88eb0eb2fbae6df9d4431a4e06ce361881e57102
GENIUS SAVAGE SHIV <83080405+SHIVGULSHAN@users.noreply.github.com>
2021-05-11 16:09:09 +0530

    Update tag.py

--- plugins/tag.py
+++ plugins/tag.py
@@ -1,9 +1,7 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# Aghori - UserBot
+# Copyright (C) 2020 TeamAghori
+# Do not kang this you kang this I will kill u.
+
 
 """
 ✘ Commands Available -
